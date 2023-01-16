f5605f62daa67ed6e41e74d9488c9fca839fece8
luk3yx <luk3yx@users.noreply.github.com>
2023-01-16 16:31:07 +1300

    Add ability to share links to code

--- main.js
+++ main.js
@@ -20,6 +20,7 @@
 "use strict";
 
 // Overridden by init.lua once it loads
+var run_playground_code;
 if (!window.run_playground_code) {
     var playground_code_tmp;
     function run_playground_code(code) {
@@ -27,10 +28,58 @@ if (!window.run_playground_code) {
     }
 }
 
-// Handle switching between tutorials
+const shareLinkElem = document.getElementById("share-link");
+shareLinkElem.addEventListener("blur", () => {
+    shareLinkElem.classList.remove("active");
+});
+
+function makeShareLink() {
+    shareLinkElem.classList.add("active");
+    shareLinkElem.value = window.location.origin + window.location.pathname +
+        '?code=' + btoa(encodeURIComponent(editor.getValue()));
+    shareLinkElem.select();
+}
+
 var editor;
+var untrustedCode = false;
+function runCode() {
+    // Warn the user if they're about to run untrusted code
+    if (untrustedCode && !confirm("Untrusted code could freeze this webpage " +
+            "and may potentially be able to break out of the Lua sandbox " +
+            "and execute arbitrary JavaScript.\n" +
+            "Are you sure you want to continue?")) {
+        return;
+    }
+
+    // Remove the untrustedCode flag and run the code
+    untrustedCode = false;
+    run_playground_code(editor.getValue());
+}
+
 const dropdown = document.getElementById("code-preset");
+var sharedCode;
+if (window.location.search.startsWith("?code=")) {
+    const option = document.createElement("option");
+    option.value = "shared";
+    option.textContent = "Shared link";
+    dropdown.appendChild(option);
+    dropdown.value = "shared";
+    sharedCode = decodeURIComponent(atob(window.location.search.slice(6)));
+}
+
+// Handle switching between tutorials
 async function updateCode() {
+    editor.setScrollPosition({scrollTop: 0});
+    if (dropdown.value === "shared") {
+        // Load in code from the URL and add a warning
+        untrustedCode = true;
+        editor.setValue(sharedCode);
+        run_playground_code("");
+        return;
+    }
+
+    // Fecth tutorial code
+    untrustedCode = false;
     editor.setValue("Loading...");
     const resp = await fetch(`tutorials/${dropdown.value}`);
     if (resp.ok) {
@@ -40,7 +89,6 @@ async function updateCode() {
     } else {
         editor.setValue(`${resp.status} error when fetching code`);
     }
-    editor.setScrollPosition({scrollTop: 0});
 }
 
 dropdown.addEventListener("change", updateCode);
@@ -60,14 +108,30 @@ require(["vs/editor/editor.main"], () => {
         scrollBeyondLastLine: false,
         wordWrap: true
     });
+
+    editor.addAction({
+        id: 'playground-run',
+        label: 'Run code',
+        contextMenuGroupId: 'playground',
+        contextMenuOrder: 1.5,
+        run: runCode,
+    });
+
+    editor.addAction({
+        id: 'playground-share',
+        label: 'Share code',
+        keybindings: [monaco.KeyMod.CtrlCmd | monaco.KeyCode.KeyS],
+        contextMenuGroupId: 'playground',
+        contextMenuOrder: 1.5,
+        run: () => makeShareLink(),
+    });
+
     updateCode();
     document.body.removeChild(document.getElementById("loader"));
 });
 
 // Handle run button clicks
-document.getElementById("run").addEventListener("click", () => {
-    run_playground_code(editor.getValue());
-});
+document.getElementById("run").addEventListener("click", runCode);
 
 // Add a confirmation message before closing the tab
 window.addEventListener("beforeunload", e => {
